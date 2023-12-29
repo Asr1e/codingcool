eb48e4f5f54505eef3ee50ed167144376785a7ff
Asr1e <mynamesyurin@gmail.com>
2023-12-29 21:52:31 +0800

    dies

--- Python/Convert-Meters-To-Kilometers.py
+++ Python/Convert-Meters-To-Kilometers.py
@@ -1,2 +1,8 @@
-def convert_distance(meters):
-    km = input("meters")*1000
+def convert_MetersToKilometers(meters):
+    KM = round(meters*1000, 2) #uses Pythons built-in/already existing round() functi>
+    return KM
+
+Meters = input("enter meters here: ")
+km = convert_MetersToKilometers(float(Meters))
+#call the converting function with inpu>
+print(f"\noutput:{km}km")
--- Python/concat-names.py
+++ Python/concat-names.py
@@ -1,8 +1,10 @@
-first_name = input()
-last_name = input()
+first_name = input("\nenter: ")
+last_name = input("\nenter: ")
 
-delimiter = input()
+delimiter = input("\nenter: ")
 
-print(f"first_name:{delimiter}:last_name")
+print(f"{first_name} {last_name}")
 
 "\John Smith"
+
+print(f"Combined = {first_name} + {delimiter} + {last_name}")
